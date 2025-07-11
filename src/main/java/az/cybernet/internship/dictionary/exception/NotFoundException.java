5ac7563bd0b2bfe629ed4741114689b908b440c0
Cahid <Bayramov.jahid@gmail.com>
2025-07-11 14:03:11 +0400

    add exception

---
+++ src/main/java/az/cybernet/internship/dictionary/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package az.cybernet.internship.dictionary.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
