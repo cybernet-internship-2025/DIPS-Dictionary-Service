b3eba53034e2882daf4006436b3b0a95245e10b0
Fuadorujlu <fuadoruclu7@gmail.com>
2025-07-11 08:01:19 +0300

    Jpa-dependency added.

--- src/main/java/az/cybernet/internship/dictionary/dictionaries.java
+++ src/main/java/az/cybernet/internship/dictionary/dictionaries.java
@@ -1,4 +1,45 @@
 package az.cybernet.internship.dictionary;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+
 public class dictionaries {
+    @Id
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    private Integer id;
+    @NotEmpty(message = "Value cannot be empty")
+    private String value;
+    private boolean isActive;
+
+    public dictionaries(Integer id, String value, boolean isActive) {
+        this.id = id;
+        this.value = value;
+        this.isActive = isActive;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
 }
--- src/main/java/az/cybernet/internship/dictionary/dictionaryRep.java
+++ src/main/java/az/cybernet/internship/dictionary/dictionaryRep.java
@@ -1,4 +1,11 @@
 package az.cybernet.internship.dictionary;
 
-public class dictionaryRep {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public interface dictionaryRep extends JpaRepository<dictionaries, Integer> {
 }
